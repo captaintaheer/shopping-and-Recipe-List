0b4858a832fc10c34d3c1d27717abadc392ebea4
captaintaheer <captaintaheer@gmail.com>
2020-09-08 22:19:48 +0800

    Improved navigation btw recipe details display

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { from } from 'rxjs';
 @Component({
@@ -7,7 +7,13 @@ import { from } from 'rxjs';
   styleUrls: ['./recipes-list.component.css'],
 })
 export class RecipesListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'This is a test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636'
+    ),
     new Recipe(
       'A test Recipe',
       'This is a test',
@@ -18,4 +24,7 @@ export class RecipesListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
